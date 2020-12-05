bb9f9a12fd08aa10d2c4b9613cd46f0c3a0fce1d
Max Blacher-L <m.blacher@web.de>
2020-12-05 16:39:44 +0100

    added Django project as submodule

--- TraLELHo/translation/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.utils.translation import ugettext as _
-
-def index(request):
-    message = _("Hello, world. You're at the translation index.")
-    return HttpResponse(message)
