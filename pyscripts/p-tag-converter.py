ebead56aab15940c23ce09879cbfc20c9d3d867d
Max Blacher-L <m.blacher@web.de>
2020-12-24 18:32:35 +0100

    pyscript uses h2

--- pyscripts/p-tag-converter.py
+++ pyscripts/p-tag-converter.py
@@ -1,3 +1,5 @@
+import re
+
 with open("./input/input_ptag.txt", "r") as f:
     lines = f.readlines()
 
@@ -17,17 +19,25 @@ tablefoot = '''
 </table>
 '''
 output = []
+sections = []
 output.append(tablehead)
 for line in lines:
     line = line.strip()
-    output.append("<tr> \n")
-    if len(line) < 20:
-        string = "<td><p>" + line + "</p></td> \n<td><p> {% translate \"" + line +"\" %} </p></td>\n"
+    if re.search('[A-Z]{4,}', line):
+        output.append(" </tbody>\n</table>\n<h2 id='" + line + "'>" + line + "</h2>\n<table class='tg'>\n <tbody>\n")
+        sections.append(line + "\n")
     else:
-        string = "<td><p>" + line + "</p></td> \n<td><p>{% blocktranslate %}\"" + line +"\"{% endblocktranslate %}</p></td>\n"
-    output.append(string)
-    output.append("</tr> \n")
+        output.append("<tr> \n")
+        if len(line) < 20:
+            string = "<td><p>" + line + "</p></td> \n<td><p> {% translate \"" + line +"\" %} </p></td>\n"
+        else:
+            string = "<td><p>" + line + "</p></td> \n<td><p>{% blocktranslate %}" + line +"{% endblocktranslate %}</p></td>\n"
+        output.append(string)
+        output.append("</tr> \n")
 output.append(tablefoot)
 
 with open("./output/p_tags.txt", "w") as f:
     f.writelines(output)
+
+with open("./output/sections.txt", "w") as f:
+    f.writelines(sections)
